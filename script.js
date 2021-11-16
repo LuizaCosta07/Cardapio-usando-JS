397026fa5d3029e6dfc35bcc93608bdfa561ea7b
Marcelo Henrique <marialuizastyles0705@gmail.com>
2021-11-16 15:32:50 -0300

    update

---
+++ script.js
@@ -0,0 +1,10 @@
+var proteina = select.options[select.selectedIndex].value;
+var bebida =  document.getElementById("bebida");
+var formaPagamento = document.getElementById();
+var pedido = ;
+
+
+function processamento(){
+    var valorTotal = document.getElementById("valorTotal");
+    media.innerHTML = valorTotal;
+}
